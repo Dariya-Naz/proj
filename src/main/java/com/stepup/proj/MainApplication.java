12def7d9740bdfc895402d9b71b9488e85a52edd
Darya Nazarova <nazarova.darya1@yandex.ru>
2025-05-29 11:50:20 +0300

    Изменена основная логика, добавлен класс "Box"

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -3,10 +3,13 @@ package com.stepup.proj;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("1001");
+        System.out.println("---");
+    }
 
-        public static void doSomething() {
-            System.out.println(1);
+    public static void doSomething() {
+        System.out.println(1);
+        System.out.println(0);
+        System.out.println(0);
+
         }
     }
-}
